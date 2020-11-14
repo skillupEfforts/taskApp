a9737903a2326b8a339220e7ffe1383e525022e6
tatsuro.hayashi <tatsuro.hayashi@imjp.co.jp>
2020-11-14 14:45:37 +0900

    vue_ボタンコンポーネントのidをv-bind、propsで親コンポーネントで変更できるように修正

--- resources/js/components/btn/BtnSubmit.vue
+++ resources/js/components/btn/BtnSubmit.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="text-center">
-        <button id="workeffortSave" type="submit" class="btn btn-primary w-50"><slot></slot></button>
+        <button v-bind:id="submitId" type="submit" class="btn btn-primary w-50"><slot></slot></button>
     </div>
 </template>
 
 <script>
 export default {
     name: 'BtnSubmit',
+    props: [
+        'submitId',
+    ],
     // data () {
     //     return {
     //         headingTtl: "タスク一覧表示画面​"
