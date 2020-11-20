c4da46087bba0c4326be459fefdb0c08717b9707
shinichiro.tamashiro <>
2020-11-20 10:47:26 +0900

    routing対応

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="wrapper">
+        <header>header</header>
+        <router-view/>
+        <footer>footer</footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+<style>
+    header {
+        width: 100%;
+        padding: 10px;
+        text-align: center;
+        background: #ccc;
+    }
+    footer {
+        width: 100%;
+        padding: 10px;
+        text-align: center;
+        background: #ccc;
+    }
+</style>
