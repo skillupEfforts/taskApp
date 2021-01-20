64d3036dd4e5f0ee11872e487435f16ee18a9c4f
tatsuro.hayashi <tatsuro.hayashi@imjp.co.jp>
2021-01-20 13:30:19 +0900

    DataTable.vue内で各タスクの実工数の取得、DBデータのjitsukosuへの上書きまで対応

--- resources/js/components/datatable/DataTable.vue
+++ resources/js/components/datatable/DataTable.vue
@@ -32,48 +32,77 @@
                                 :name="'actualHour' + dbData.taskname"
                                 :id="'actualHour' + dbData.taskname"
                                 class="form-control"
-                                v-model="sendDbTaskData[dbData.taskname]"
+                                v-model="tasksHourValue[dbData.taskname]"
                                 @input="$emit('input', $event.target.value)"
                                 placeholder="実工数を入力してください。">
-                            {{ sendDbTaskData[dbData.taskname] }}
+                            <!-- {{ 'tasksHourValue' + dbData.taskname }} -->
                         </td>
                     </tr>
                 </tbody>
             </table>
         </div>
         <div class="l-w50-center mt-5">
-            <BtnSubmit submit-id="SubmitHours" button-type="submit" @onClick="saveHours">実工数保存</BtnSubmit>
+            <BtnSubmit submit-id="SubmitHours" button-type="submit" @onClick="saveHours(), hour()">実工数保存</BtnSubmit>
         </div>
     </div>
 </template>
 
 <script>
 import BtnSubmit from '../btn/BtnSubmit.vue';
+// :value="`tasksHourValue` + dbData.taskname"
 
 export default {
     name: 'DataTable',
     data () {
         return {
-            // sendDbTaskData: Array,
+            cloneDbData: {//DBデータのコピー
+                type: Object,
+                required: false
+            },
+            tasksHourValue: []//実工数の配列（中身はオブジェクト）
         }
     },
-    model: {
-        prop: 'actualHour',
-        event: 'input'
-    },
+    // model: {
+    //     prop: 'tasksHourValue',
+    //     event: 'input'
+    // },
     props: {
         sendDbTaskData: Array,
-        actualHourValue: String,
+        // cloneDbData: Object,
+        // tasksHourValue: String,
+    },
+    updated() {//親からのDBデータをコピーする
+        // console.log(this.cloneDbData)
     },
     mounted() {
+        // console.log(typeof this.tasksHourValue)
     },
     computed: {
-
+        // getHour() {
+        //     // var self = this
+        //     return console.log(this.tasksHourValue)
+        // }
     },
     methods: {
-        saveHours () {
-            // alert('工数保存しました。')
-            // console.log(this.sendDbTaskData.task3);
+        saveHours () {//DBデータのコピーを使用して、実工数配列にオブジェクトとして入れる
+            // console.log(this.cloneDbData)
+            this.cloneDbData = [...this.sendDbTaskData]
+
+            this.cloneDbData.forEach((element, index) => {
+                this.tasksHourValue.splice(index, 0, {[element.taskname]: this.tasksHourValue[element.taskname]})
+                this.cloneDbData[index].jitsukosu = this.tasksHourValue[index][element.taskname] + this.cloneDbData[index].kosu
+                // this.tasksHourValue.push({[element.taskname]: this.tasksHourValue[element.taskname]})
+                // console.log(this.tasksHourValue[index])
+                // this.tasksHourValu[element.taskname] = this.tasksHourValu[element.taskname]
+                // this.$set(this.cloneDbData, this.tasksHourValue[element.taskname], this.tasksHourValue[index][element.taskname])
+            })
+            console.log(this.tasksHourValue)
+            console.log(this.cloneDbData)
+            // console.log(this.cloneDbData)
+
+        },
+        hour() {
+            // console.log(this.tasksHourValue)
         }
     },
     components: {
