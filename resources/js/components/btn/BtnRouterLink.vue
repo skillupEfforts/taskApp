2237a803f16aaeeff69df582c7d7bd64dcc1bd86
tatsuro.hayashi <tatsuro.hayashi@imjp.co.jp>
2021-04-02 18:14:30 +0900

    routerlinkのボタンをコンポーネント化

---
+++ resources/js/components/btn/BtnRouterLink.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mt-5 text-center">
+        <router-link class="btn btn-secondary w-50" :to="to"><slot></slot></router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BtnRouterLink',
+    props: {
+        to: String,
+    }
+}
+</script>
