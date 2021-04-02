de42ac1ad2573180d588d33834febda8112eb54f
tatsuro.hayashi <tatsuro.hayashi@imjp.co.jp>
2021-04-02 18:08:11 +0900

    PageLoginに関わるコンポーネントの格納先を変更

---
+++ resources/js/components/form/login/FormLoginSubmitBtn.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center mt-3">
+        <button :id="loginSubmitBtnId" :type="loginSubmitBtnType" class="btn btn-primary w-100"><slot></slot></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FormLoginSubmitBtn',
+    props: {
+        loginSubmitBtnId: String,
+        loginSubmitBtnType: String
+    }
+}
+</script>
