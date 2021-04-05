0a616452536a8e1fe5d9c181fa0571389b9a96cc
tatsuro.hayashi <tatsuro.hayashi@imjp.co.jp>
2021-04-05 16:12:24 +0900

    🔧 lintの設定ファイルを修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,20 @@
 module.exports = {
     env: {
         browser: true,
-        es2021: true
+        es2021: true,
+        node: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:vue/essential',
-        'plugin:vue/recommended',
-        'prettier'
-    ],
+    extends: ['eslint:recommended', 'plugin:vue/essential', 'plugin:vue/recommended', 'prettier'],
     parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module'
+        ecmaVersion: 2021,
+        sourceType: 'module',
     },
-    plugins: [
-        'vue'
-    ],
+    plugins: ['vue'],
     rules: {
-        'quotes': ['warn', 'single']
-    }
+        quotes: ['warn', 'single'], // シングルクォートで囲む
+        eqeqeq: 2, // 厳密等価演算子の使用のみを許可
+        'no-var': 2, // varは禁止
+        'no-console': 0, // コンソールは許可
+        'vue/html-closing-bracket-newline': [2, { multiline: 'never' }], // "never" ...閉じ括弧の前の改行を禁止します。
+    },
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-    trailingComma: "es5",
-    tabWidth: 2,
+    trailingComma: 'es5',
+    tabWidth: 4,
     semi: false,
     singleQuote: true,
     printWidth: 120,
+    bracketSpacing: true,
     overrides: [
         {
-        files: '*.vue',
-        options: {
-            semi: false,
-            singleQuote: true,
-        }
+            files: '*.vue',
+            options: {
+                semi: false,
+                singleQuote: true,
+            },
         },
-    ]
-};
+    ],
+}
